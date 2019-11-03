7dd971913e82950a3da155bc61c7516c8bea1b3e
Frank <1018433655@qq.com>
2019-11-03 10:23:38 +0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='es_pandas',
+    version='0.0.1',
+    author='Frank',
+    author_email='fu.frank@foxmail.com',
+    description='Read, write and update large scale pandas DataFrame with ElasticSearch',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/fuyb1992/es_pandas',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
