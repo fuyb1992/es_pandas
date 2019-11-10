d497ea63805945b93ee50f9322dea27acdfbdf5f
Frank <1018433655@qq.com>
2019-11-10 09:35:52 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as fh:
 
 setuptools.setup(
     name='es_pandas',
-    version='0.0.1',
+    version='0.0.2',
     author='Frank',
     author_email='fu.frank@foxmail.com',
     description='Read, write and update large scale pandas DataFrame with ElasticSearch',
