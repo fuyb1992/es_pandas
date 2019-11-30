f8b9601d13196ca29e4c16b749415c0a8ea29099
Frank <1018433655@qq.com>
2019-11-30 14:21:25 +0800

    Update test.py

--- test.py
+++ test.py
@@ -22,20 +22,21 @@ doc_type = 'demo'
 ep.init_es_tmpl(df, doc_type)
 
 # waiting for es template writing
-time.sleep(10)
+time.sleep(30)
 
 # Example of write data to es, auto create and put template to es if template does not exits
 ep.to_es(df, index)
 
 # waiting for es data writing
-time.sleep(10)
+time.sleep(30)
 # get certain fields from es, set certain columns dtype
 heads = ['Num', 'Date', 'Alpha']
 dtype = {'Num': 'float', 'Alpha': object}
-df = ep.to_pandas(index, heads=heads, dtype=dtype)
+df = ep.to_pandas(index)
 print(df.head())
 print(df.dtypes)
 
+time.sleep(30)
 df2 = pd.DataFrame({'Alpha': [chr(i) for i in range(97, 129)],
                     'Num': [x for x in range(32)],
                     'Date': pd.date_range(start='2019/01/01', end='2019/02/01')})
