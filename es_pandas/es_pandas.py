64916ad3d91e3613bbd6c7cf94e198a1e7825a8d
fuyb1992 <1018433655@qq.com>
2020-06-14 15:21:50 +0800

    Add doc _id to dataframe index

--- es_pandas/es_pandas.py
+++ es_pandas/es_pandas.py
@@ -56,9 +56,16 @@ class es_pandas(object):
 
         return success_num
 
-    def get_source(self, anl):
-        for mes in anl:
-            yield mes['_source']
+    def get_source(self, anl, show_progress=False, count=0):
+        if show_progress:
+            with progressbar.ProgressBar(max_value=count) as bar:
+                for i in range(count):
+                    mes = next(anl)
+                    yield {'_id': mes['_id'], **mes['_source']}
+                    bar.update(i)
+        else:
+            for mes in anl:
+                yield {'_id': mes['_id'], **mes['_source']}
 
     def infer_dtype(self, index, heads):
         if self.es7:
@@ -74,7 +81,7 @@ class es_pandas(object):
         return dtype
 
 
-    def to_pandas(self, index, query_rule=None, heads=[], dtype={}, infer_dtype=False, **kwargs):
+    def to_pandas(self, index, query_rule=None, heads=[], dtype={}, infer_dtype=False, show_progress=True, **kwargs):
         """
         scroll datas from es, and convert to dataframe, the index of dataframe is from es index,
         about 2 million records/min
@@ -86,6 +93,7 @@ class es_pandas(object):
             heads: certain columns get from es fields, [] for all fields
             dtype: dict like, pandas dtypes for certain columns
             infer_dtype: bool, default False, if true, get dtype from es template
+            show_progress: bool, default True, if true, show progressbar on console
         Returns:
             DataFrame
         """
@@ -96,7 +104,7 @@ class es_pandas(object):
             raise Exception('Empty for %s' % index)
         query_rule['_source'] = heads
         anl = helpers.scan(self.es, query=query_rule, index=index, **kwargs)
-        df = pd.DataFrame(self.get_source(anl))
+        df = pd.DataFrame(self.get_source(anl, show_progress=show_progress, count=count)).set_index('_id')
         if infer_dtype:
             dtype = self.infer_dtype(index, df.columns.values)
         if len(dtype):
@@ -121,44 +129,6 @@ class es_pandas(object):
                                            self.rec_to_actions(df, index, doc_type, chunk_size=chunk_size, _op_type='delete'),
                                            thread_count=thread_count, chunk_size=chunk_size)
         return np.sum([res[0] for res in delete_gen])
-
-    def update_es(self, df, index, doc_type, key_col='', chunk_size=1000, thread_count=2):
-        '''
-
-        :param df: DataFrame you want to update in elasticsearch
-        :param index: elasticsearch index
-        :param doc_type: elasticsearch doc type
-        :param key_col: default use DataFrame index
-        :param chunk_size:
-        :param thread_count:
-        :return: number of update records
-        '''
-        assert isinstance(key_col, str)
-        raise Exception('to be continued')
-
-    def to_es_dev(self, df, index, key_col, ignore_cols=[], append=False, doc_type=None, delete=False, thread_count=2,
-                  chunk_size=1000, request_timeout=60, success_threshold=0.9):
-        '''
-
-        :param df: pandas DataFrame data
-        :param index: full name of es indices
-        :param doc_type: full name of es template
-        :param delete: delete existing doc_type template if True
-        :param thread_count: number of thread sent data to es
-        :param chunk_size: number of docs in one chunk sent to es
-        :param request_timeout:
-        :param success_threshold:
-        :return: num of the number of data written into es successfully
-        '''
-        if np.sum(df.duplicated(subset=key_col)) > 0:
-            raise Exception('Duplicated data exits in key column: %s' % key_col)
-
-        if not self.ic.exists(index):
-            self.to_es(df, index, doc_type=doc_type, delete=delete, thread_count=thread_count,
-                       chunk_size=chunk_size, request_timeout=request_timeout, success_threshold=success_threshold)
-        else:
-            self.update_to_es(df, index, key_col, ignore_cols=ignore_cols, append=append, doc_type=doc_type,
-                              thread_count=thread_count, chunk_size=chunk_size, success_threshold=success_threshold)
 
     def rec_to_actions(self, df, index, doc_type, use_index=False, chunk_size=1000, _op_type=None):
         for i in progressbar.progressbar(range(math.ceil(len(df) / chunk_size))):
