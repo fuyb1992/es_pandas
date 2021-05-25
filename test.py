3489f748cb06e54a625790cdc7258f09bad7467e
Frank <1018433655@qq.com>
2021-05-25 15:37:59 +0800

    Update test.py

--- test.py
+++ test.py
@@ -28,14 +28,14 @@ ep.to_es(df, index, doc_type=doc_type, thread_count=2, chunk_size=5000)
 print('write es doc without index finished')
 
 # Example of use DataFrame index as es doc _id
-ep.to_es(df, index, doc_type=doc_type, use_index=True, thread_count=2, chunk_size=5000, show_progress=False)
+ep.to_es(df, index, doc_type=doc_type, use_index=True, thread_count=2, chunk_size=1000, show_progress=False)
 print('write es doc with index finished')
 
 # waiting for es data writing
 time.sleep(5)
 
 # Delete doc by doc _id
-ep.to_es(df.iloc[5000:], index, doc_type=doc_type, _op_type='delete', thread_count=2, chunk_size=5000)
+ep.to_es(df.iloc[5000:], index, doc_type=doc_type, _op_type='delete', thread_count=2, chunk_size=1000)
 print('delete es doc finished')
 
 # waiting for es data writing
@@ -44,7 +44,7 @@ time.sleep(5)
 # Update doc by doc _id
 df.iloc[:1000, 1] = 'Bye'
 df.iloc[:1000, 2] = pd.datetime.now()
-ep.to_es(df.iloc[:1000, 1:], index, doc_type=doc_type, _op_type='update', thread_count=2, chunk_size=5000)
+ep.to_es(df.iloc[:1000, 1:], index, doc_type=doc_type, _op_type='update', thread_count=2, chunk_size=1000)
 print('update es doc finished')
 
 # waiting for es data writing
@@ -62,5 +62,5 @@ df = ep.to_pandas(index, infer_dtype=True, size=500)
 print(df.dtypes)
 
 # Example of write data to es with pandas.io.json
-ep.to_es(df, index, doc_type=doc_type, use_pandas_json=True, thread_count=2, chunk_size=5000)
+ep.to_es(df, index, doc_type=doc_type, use_pandas_json=True, thread_count=2, chunk_size=1000)
 print('write es doc with pandas.io.json finished')
